ff1e2f5068c8654ee0cf419ffe8d2da7c5b6b788
grafillo <hotgraf@list.ru>
2023-11-23 13:06:42 +0300

    ready

--- sql.php
+++ sql.php
@@ -7,10 +7,6 @@ $sql = "SELECT users.id,users.name, groups_tb.name AS name_group FROM groups_tb 
       WHERE groups_tb.id ='1'";
 
 
-//$sql = "SELECT name AS name_group FROM groups_tb
-//      WHERE groups_tb.id ='1'";
-
-
 $result = mysqli_query($connect, $sql);
 $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
